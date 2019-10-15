3c7dbeef462b7920ca84ce43ac393df4103dd16f
KoudaiAbe <52918698+KoudaiAbe@users.noreply.github.com>
2019-10-15 15:18:21 +0900

    Create FruitServlet.java

---
+++ output/FruitServlet.java
@@ -0,0 +1,41 @@
+package ex;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FruitServlet
+ */
+@WebServlet("/FruitServlet")
+public class FruitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response)
+					throws ServletException, IOException {
+
+		//create Fruit instance
+		Fruit f = new Fruit("いちご",700);
+
+		//applicationscope
+		ServletContext application = this.getServletContext();
+		application.setAttribute("fruit", f);
+
+		//Forward
+		RequestDispatcher dispathcher =
+				request.getRequestDispatcher("/WEB-INF/ex/fruit.jsp");
+		dispathcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response)
+					throws ServletException, IOException {
+	}
+}
