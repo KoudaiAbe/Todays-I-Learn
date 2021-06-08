66917d6f2d3dffa1c9eefa217d1bedbf05dc99c2
KoudaiAbe <52918698+KoudaiAbe@users.noreply.github.com>
2021-06-08 21:11:01 +0900

    Update スクレイピング.py

--- output/スクレイピング.py
+++ output/スクレイピング.py
@@ -8,6 +8,9 @@ soup = BeautifulSoup(res.content, "lxml", from_encoding='utf-8')
 '''
 name = soup.find_all('small')
 
+class_を指定して要素を取得する場合
+name = soup.find_all('', class_='cat')
+
 for umamusume_name in  name:
   output_name = umamusume_name.text.replace('\n', '')
   print(output_name)
@@ -18,3 +21,10 @@ birth = soup.find_all('strong')
 for umamusume_birth in  birth:
   output_birth = umamusume_birth.text.replace('\n', '')
   print(output_birth)
+
+  '''
+  参考
+  https://qiita.com/itkr/items/513318a9b5b92bd56185
+  
+  
+  '''
